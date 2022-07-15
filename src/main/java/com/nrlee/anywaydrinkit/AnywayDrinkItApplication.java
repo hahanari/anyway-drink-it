34e2dcd95859f70c8698cbdea7cbe7520b56a938
이나리/검색개발팀 <nr.lee@musinsa.com>
2022-07-15 20:03:17 +0900

    feat: Add project

---
+++ src/main/java/com/nrlee/anywaydrinkit/AnywayDrinkItApplication.java
@@ -0,0 +1,13 @@
+package com.nrlee.anywaydrinkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnywayDrinkItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnywayDrinkItApplication.class, args);
+    }
+
+}
