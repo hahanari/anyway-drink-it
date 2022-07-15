7aed4fa89971376f93c6c4494b4d0d9a1b05ddb4
hahanari <hahanari@hanmail.net>
2022-07-15 20:03:17 +0900

    feat: Add project

---
+++ src/test/java/com/nrlee/anywaydrinkit/AnywayDrinkItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrlee.anywaydrinkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnywayDrinkItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
