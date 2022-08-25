ab6f5120822119f9b94cf09c4c70e9ac22d8f9b5
hahanari <hahanari@hanmail.net>
2022-08-25 17:24:39 +0900

    Docs: 배치 job 완료 시 프로세스 종료 로직 추가
    `JobExecutionListener` 추가

--- batch/src/main/java/com/nrlee/batch/job/wine/v1/WineCreateBatch.java
+++ batch/src/main/java/com/nrlee/batch/job/wine/v1/WineCreateBatch.java
@@ -1,7 +1,6 @@
 package com.nrlee.batch.job.wine.v1;
 
 import java.util.Collections;
-
 import javax.annotation.Resource;
 
 import com.nrlee.batch.config.UniqueRunIdIncrementer;
@@ -12,6 +11,8 @@ import com.nrlee.batch.job.wine.v1.repository.WineRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
@@ -52,6 +53,7 @@ public class WineCreateBatch {
                 .next(bulkWine())
                 .next(setRefreshInterval("30s"))
                 .next(rebindReadAlias())
+                .listener(listener())
                 .build();
     }
 
@@ -106,5 +108,21 @@ public class WineCreateBatch {
                 .name("repositoryItemReader")
                 .sorts(Collections.singletonMap("id", Sort.Direction.ASC))
                 .build();
+    }
+
+    @Bean
+    public JobExecutionListener listener() {
+        return new JobExecutionListener() {
+            @Override
+            public void beforeJob(JobExecution jobExecution) {
+                log.warn("Job Start !!!");
+            }
+
+            @Override
+            public void afterJob(JobExecution jobExecution) {
+                log.warn("Job End !!!");
+                System.exit(1);
+            }
+        };
     }
 }
